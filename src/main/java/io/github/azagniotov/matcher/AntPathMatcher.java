ebc6c04b8fe2a22db8eea9749d5a5f78a6ac68f2
Alexander Zagniotov <azagniotov@gmail.com>
2016-09-12 11:03:34 -0700

    Update AntPathMatcher.java
    Code clean-up

--- src/main/java/io/github/azagniotov/matcher/AntPathMatcher.java
+++ src/main/java/io/github/azagniotov/matcher/AntPathMatcher.java
@@ -6,19 +6,34 @@ package io.github.azagniotov.matcher;
  * '?' - matches one character
  * '*' - matches zero or more characters
  * '**' - matches zero or more directories in a path
+ * <p>
+ * The instances of this class can be configured via its {@link Builder} to:
+ * (1) Use a custom path separator. The default is '/' character
+ * (2) Ignore character case during comparison. The default is case-sensitive
+ * <p>
+ * The custom path separator & ignoring character case options were inspired by Spring's AntPathMatcher
  */
+@SuppressWarnings("WeakerAccess")
 public class AntPathMatcher {
 
     private static final char ASTERISK = '*';
     private static final char QUESTION = '?';
-    public static final char PATH_SEPARATOR = '/';
+    private static final int ASCII_CASE_DIFFERENCE = 32;
+
+    private final char pathSeparator;
+    private final boolean ignoreCase;
+
+    private AntPathMatcher(final char pathSeparator, boolean ignoreCase) {
+        this.pathSeparator = pathSeparator;
+        this.ignoreCase = ignoreCase;
+    }
 
     public boolean isMatch(final String pattern, final String path) {
         if (pattern.isEmpty()) {
             return path.isEmpty();
         } else if (pattern.length() == 1 && pattern.charAt(0) == ASTERISK) {
-            return path.isEmpty() || path.charAt(0) != PATH_SEPARATOR && isMatch(pattern, path.substring(1));
-        } else if (path.isEmpty() && pattern.charAt(0) == PATH_SEPARATOR) {
+            return path.isEmpty() || path.charAt(0) != pathSeparator && isMatch(pattern, path.substring(1));
+        } else if (path.isEmpty() && pattern.charAt(0) == pathSeparator) {
             return doubleAsteriskMatch(pattern.substring(1), path);
         }
 
@@ -38,8 +53,18 @@ public class AntPathMatcher {
             return isMatch(pattern.substring(1), path.substring(start));
         }
 
-        return !path.isEmpty() && (path.charAt(0) == patternStart || patternStart == QUESTION)
+        return !path.isEmpty() && (equal(path.charAt(0), patternStart) || patternStart == QUESTION)
                 && isMatch(pattern.substring(1), path.substring(1));
+    }
+
+    private boolean equal(final char pathChar, final char patternChar) {
+        if (ignoreCase) {
+            return pathChar == patternChar ||
+                    ((pathChar > patternChar) ?
+                            pathChar == patternChar + ASCII_CASE_DIFFERENCE :
+                            pathChar == patternChar - ASCII_CASE_DIFFERENCE);
+        }
+        return pathChar == patternChar;
     }
 
     private boolean doubleAsteriskMatch(final String pattern, final String path) {
@@ -49,17 +74,36 @@ public class AntPathMatcher {
             return true;
         }
 
-        int pointer = movePointer(path);
+        int pointer = 0;
+        while (pointer < path.length() && (path.charAt(pointer) != pathSeparator)) {
+            pointer++;
+        }
         return (isMatch(pattern.substring(2), path.substring(pointer)) ||
                 (pointer < path.length() && doubleAsteriskMatch(pattern, path.substring(pointer + 1))));
 
     }
 
-    private int movePointer(final String path) {
-        int pointer = 0;
-        while (pointer < path.length() && (path.charAt(pointer) != PATH_SEPARATOR)) {
-            pointer++;
+    public static final class Builder {
+
+        private char pathSeparator = '/';
+        private boolean ignoreCase = false;
+
+        public Builder() {
+
         }
-        return pointer;
+
+        public Builder withPathSeparator(final char pathSeparator) {
+            this.pathSeparator = pathSeparator;
+            return this;
+        }
+
+        public Builder withIgnoreCase(final boolean ignoreCase) {
+            this.ignoreCase = ignoreCase;
+            return this;
+        }
+
+        public AntPathMatcher build() {
+            return new AntPathMatcher(pathSeparator, ignoreCase);
+        }
     }
 }

1222917827d2ccaf9952ef8c51e8b53e860d6c3f
Alexander Zagniotov <azagniotov@gmail.com>
2016-09-12 19:19:08 -0700

    Renamed a method & code clean-up

--- src/main/java/io/github/azagniotov/matcher/AntPathMatcher.java
+++ src/main/java/io/github/azagniotov/matcher/AntPathMatcher.java
@@ -6,11 +6,11 @@ package io.github.azagniotov.matcher;
  * '?' - matches one character
  * '*' - matches zero or more characters
  * '**' - matches zero or more directories in a path
- * <p>
+ * <p/>
  * The instances of this class can be configured via its {@link Builder} to:
  * (1) Use a custom path separator. The default is '/' character
  * (2) Ignore character case during comparison. The default is case-sensitive
- * <p>
+ * <p/>
  * The custom path separator & ignoring character case options were inspired by Spring's AntPathMatcher
  */
 @SuppressWarnings("WeakerAccess")
@@ -28,11 +28,11 @@ public class AntPathMatcher {
         this.ignoreCase = ignoreCase;
     }
 
-    public boolean isMatch(final String pattern, final String path) {
+    public boolean match(final String pattern, final String path) {
         if (pattern.isEmpty()) {
             return path.isEmpty();
         } else if (pattern.length() == 1 && pattern.charAt(0) == ASTERISK) {
-            return path.isEmpty() || path.charAt(0) != pathSeparator && isMatch(pattern, path.substring(1));
+            return path.isEmpty() || path.charAt(0) != pathSeparator && match(pattern, path.substring(1));
         } else if (path.isEmpty() && pattern.charAt(0) == pathSeparator) {
             return doubleAsteriskMatch(pattern.substring(1), path);
         }
@@ -45,16 +45,16 @@ public class AntPathMatcher {
 
             int start = 0;
             while (start < path.length()) {
-                if (isMatch(pattern.substring(1), path.substring(start))) {
+                if (match(pattern.substring(1), path.substring(start))) {
                     return true;
                 }
                 start++;
             }
-            return isMatch(pattern.substring(1), path.substring(start));
+            return match(pattern.substring(1), path.substring(start));
         }
 
         return !path.isEmpty() && (equal(path.charAt(0), patternStart) || patternStart == QUESTION)
-                && isMatch(pattern.substring(1), path.substring(1));
+                && match(pattern.substring(1), path.substring(1));
     }
 
     private boolean equal(final char pathChar, final char patternChar) {
@@ -70,7 +70,7 @@ public class AntPathMatcher {
     private boolean doubleAsteriskMatch(final String pattern, final String path) {
         if (pattern.length() == 1 || pattern.charAt(1) != ASTERISK) {
             return false;
-        } else if (pattern.length() == 2 || isMatch(pattern.substring(3), path)) {
+        } else if (pattern.length() == 2 || match(pattern.substring(3), path)) {
             return true;
         }
 
@@ -78,7 +78,7 @@ public class AntPathMatcher {
         while (pointer < path.length() && (path.charAt(pointer) != pathSeparator)) {
             pointer++;
         }
-        return (isMatch(pattern.substring(2), path.substring(pointer)) ||
+        return (match(pattern.substring(2), path.substring(pointer)) ||
                 (pointer < path.length() && doubleAsteriskMatch(pattern, path.substring(pointer + 1))));
 
     }
@@ -97,8 +97,8 @@ public class AntPathMatcher {
             return this;
         }
 
-        public Builder withIgnoreCase(final boolean ignoreCase) {
-            this.ignoreCase = ignoreCase;
+        public Builder withIgnoreCase() {
+            this.ignoreCase = true;
             return this;
         }
 
